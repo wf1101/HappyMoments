c3102c3ffb78759ead3ebfad34237b489332f96a
Wenjie <traxiaojie@gmail.com>
2018-07-11 22:34:23 -0700

    Create LoginActivity and xml file

---
+++ app/src/main/java/com/happymoments/wenjie/happymoments/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.happymoments.wenjie.happymoments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText mEmail;
+    private EditText mPassword;
+    private Button mLoginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mEmail = findViewById(R.id.email_field);
+        mPassword = findViewById(R.id.password_field);
+        mLoginBtn = findViewById(R.id.login_btn);
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // existing user login successfully
+
+
+                // new user - create a new user in database
+
+
+            }
+        });
+
+    }
+}
