f1418ae68a2b03ea6c989806cc121ee6fdb9fa78
Wenjie <traxiaojie@gmail.com>
2018-07-12 20:12:06 -0700

    Retrieve simple data from firebase and display on profile screen

--- app/src/main/java/com/happymoments/wenjie/happymoments/ProfileActivity.java
+++ app/src/main/java/com/happymoments/wenjie/happymoments/ProfileActivity.java
@@ -1,15 +1,34 @@
 package com.happymoments.wenjie.happymoments;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProfileActivity extends AppCompatActivity {
+//    private FirebaseDatabase mDatabase;
+    private MomentAdapter mMomentAdapter;
+    private ListView mMomentList;
+    private DatabaseReference mDataReference;
+    private ChildEventListener mChildEventListener;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +45,42 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        // Initialize message ListView and its adapter
+        mMomentList = findViewById(R.id.moment_list);
+        List<Moment> myMoments = new ArrayList<>();
+        mMomentAdapter = new MomentAdapter(this, R.layout.moment_display, myMoments);
+        mMomentList.setAdapter(mMomentAdapter);
+
+        mChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Moment newMoment = dataSnapshot.getValue(Moment.class);
+                mMomentAdapter.add(newMoment);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+
+        mDataReference = FirebaseDatabase.getInstance().getReference().child("moment");
+        mDataReference.addChildEventListener(mChildEventListener);
 
     }
 }
