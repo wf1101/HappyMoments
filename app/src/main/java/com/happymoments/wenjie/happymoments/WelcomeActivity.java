0b41309d29c13bf343f536aba0f7a20327029a4d
Wenjie <traxiaojie@gmail.com>
2018-07-20 15:44:29 -0700

    Modified splash screen

--- app/src/main/java/com/happymoments/wenjie/happymoments/WelcomeActivity.java
+++ app/src/main/java/com/happymoments/wenjie/happymoments/WelcomeActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class HomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
     // Welcome screen time 2.2s
     private static int SPLASH_TIME_OUT = 2200;
 
@@ -13,13 +15,19 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+
+        // full screen display
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_welcome);
 
         // load Welcome screen
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent mainIntent = new Intent(HomeActivity.this, MainActivity.class);
+                Intent mainIntent = new Intent(WelcomeActivity.this, MainActivity.class);
                 startActivity(mainIntent);
                 finish();
             }
